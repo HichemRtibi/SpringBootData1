564030e34293f1792b1a1dee0dc99c18e1cd2165
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-24 03:08:06 +0100

    spring data with or

--- src/main/java/com/hichem/rtibi/DemoApplication.java
+++ src/main/java/com/hichem/rtibi/DemoApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
@@ -25,16 +27,21 @@ public class DemoApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Product product = new Product("product1", "GENERAL", "category1", "description1", 19.00);
 
-        Product product2 = new Product("product2", "CUSTOM", "category2", "description1", 12.00);
+        Product product2 = new Product("product2", "CUSTOM", "category2", "description3", 12.00);
 
-        Product product3 = new Product("product1", "SPECEFIC", "category3", "description1", 10.00);
+        Product product3 = new Product("product1", "SPECEFIC", "category1", "description1", 10.00);
         productRepository.save(product);
         productRepository.save(product2);
         productRepository.save(product3);
 
 
-        Product productbYtYPE=productRepository.findByName("product2");
-        logger.info("product By Name :"+productbYtYPE.toString());
+       // Product productbYtYPE=productRepository.findByName("product2");
+        //logger.info("product By Name :"+productbYtYPE.toString());
+
+        List<Product> products=productRepository.findByCategoryOrDescription("category1","description3");
+        for (Product product1:products) {
+            logger.info("List product byCategory and description :"+product1.toString());
+        }
     }
 
 }
--- src/main/java/com/hichem/rtibi/repositories/ProductRepository.java
+++ src/main/java/com/hichem/rtibi/repositories/ProductRepository.java
@@ -4,8 +4,12 @@ import com.hichem.rtibi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 
     public Product findByName(String name);
+
+    public List<Product> findByCategoryOrDescription(String category, String description);
 }
