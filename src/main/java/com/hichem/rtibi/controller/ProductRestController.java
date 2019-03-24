282a712886ec0ffb37d8036a113f1cfb70bb3281
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-24 17:35:45 +0100

    update product with put method

--- src/main/java/com/hichem/rtibi/controller/ProductRestController.java
+++ src/main/java/com/hichem/rtibi/controller/ProductRestController.java
@@ -2,6 +2,8 @@ package com.hichem.rtibi.controller;
 
 import com.hichem.rtibi.model.Product;
 import com.hichem.rtibi.repositories.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping(path = "api/products/")
 public class ProductRestController {
+    Logger logger=LoggerFactory.getLogger(ProductRestController.class);
     @Autowired
     private ProductRepository productRepository;
 
@@ -22,5 +25,22 @@ public class ProductRestController {
     @RequestMapping(method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
     public Product SaveProduct(@RequestBody Product product){
         return productRepository.save(product);
+    }
+    @RequestMapping(path = "{id}",method = RequestMethod.PUT,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Product productUpdate(@RequestBody Product product,@PathVariable("id") String id){
+        Product productFound=productRepository.findById(id).orElse(null);
+        if(productFound!=null){
+            productFound.setName(product.getName());
+            productFound.setPrice(product.getPrice());
+            productFound.setCategory(product.getCategory());
+            productFound.setType(product.getType());
+            productFound.setDescription(product.getDescription());
+            return productRepository.save(productFound);
+        }
+        else{
+            logger.info("product Not found");
+        }
+        return product;
+
     }
 }
