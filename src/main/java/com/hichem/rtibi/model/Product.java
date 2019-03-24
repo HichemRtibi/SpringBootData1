343eeaaf8ba032180ff94e78c8fab2aa59b2555d
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-24 02:40:42 +0100

    generateed value and genericGenerateur

--- src/main/java/com/hichem/rtibi/model/Product.java
+++ src/main/java/com/hichem/rtibi/model/Product.java
@@ -1,12 +1,15 @@
 package com.hichem.rtibi.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Products")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid",strategy = "uuid2")
     private String id;
     private String name;
     private String type;
